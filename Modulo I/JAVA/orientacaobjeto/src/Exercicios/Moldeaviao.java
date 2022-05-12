a74c4c50056234de3a9628d988482fc42621c7b1
DAIANE <mdaianesilva1@gmail.com>
2022-05-11 21:33:25 -0300

    orinetacaoobjeto

---
+++ Modulo I/JAVA/orientacaobjeto/src/Exercicios/Moldeaviao.java
@@ -0,0 +1,21 @@
+package Exercicios;
+
+public class Moldeaviao {
+	
+	String cor;
+	int pessoas;
+	double combustivel;
+	boolean voando;
+	
+	public void escrever()
+	{
+	
+	if (this.voando==true)
+		System.out.print ("O aviao esta voando");
+	
+	
+	
+	
+
+}
+}
